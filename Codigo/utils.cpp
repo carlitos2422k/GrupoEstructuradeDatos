fa37820ecf1b47c930321c60efb98a39575ef0d0
carlitos2422k <carlospe0505@gmail.com>
2024-12-12 18:50:11 -0500

    Pruebas escritorio

--- Codigo/utils.cpp
+++ Codigo/utils.cpp
@@ -20,7 +20,7 @@ bool Utils::validateId(const std::string& idInput) {
         return false;
     }
 
-    long id = std::stol(idInput);  // Convierte el string a un número entero largo
+    long id = std::stol(idInput);  
     std::vector<long> digits(10);
     long remainder;
     long doubledDigit;
@@ -29,13 +29,13 @@ bool Utils::validateId(const std::string& idInput) {
     long totalSum;
     long checkDigit;
 
-    // Extraer los dígitos individuales del número
+    
     for (int i = 9; i >= 0; --i) {
         digits[i] = id % 10;
         id /= 10;
     }
 
-    // Procesar los dígitos en las posiciones pares
+    
     for (int i = 0; i < 9; i += 2) {
         doubledDigit = digits[i] * 2;
         if (doubledDigit > 9) {
@@ -44,12 +44,12 @@ bool Utils::validateId(const std::string& idInput) {
         evenPositionSum += doubledDigit;
     }
 
-    // Procesar los dígitos en las posiciones impares
+    
     for (int i = 1; i < 9; i += 2) {
         oddPositionSum += digits[i];
     }
 
-    // Calcular la suma total y el dígito verificador
+  
     totalSum = evenPositionSum + oddPositionSum;
     remainder = totalSum % 10;
     checkDigit = 10 - remainder;
@@ -57,7 +57,7 @@ bool Utils::validateId(const std::string& idInput) {
         checkDigit = 0;
     }
 
-    // Comparar el dígito verificador calculado con el último dígito
+    
     return checkDigit == digits[9];
 }
 
@@ -77,11 +77,11 @@ void Utils::modifyCharInString(std::string& str, char target, int offset) {
     }
 }
 bool Utils::validarPlacaEcuador(const std::string& placa) {
-    // Excluimos D, F, H y V solo en la primera letra.
+   
     std::regex regexParticular("^[A-CEG-IK-UX-Z][A-Z]{2}-[0-9]{4}$");
     std::regex regexMoto("^[A-CEG-IK-UX-Z][A-Z]{2}-[0-9]{3}$");
 
-    // Validamos la placa contra las expresiones regulares.
+   
     if (std::regex_match(placa, regexParticular) || std::regex_match(placa, regexMoto)) {
         return true;
     }
@@ -100,41 +100,6 @@ void Utils::mostrarMenu(const std::string opciones[], int numOpciones, int selec
         }
     }
 }
-void Utils::mostrarMenuManual(const std::string opciones[], int numOpciones, int seleccion) {
-    system("cls");
-    std::cout << "\033[35m*********** Manual de usuario ***********\033[0m\n";
-    for (int i = 0; i < numOpciones; ++i) {
-        if (i == seleccion) {
-            std::cout << "\033[32m > " << opciones[i] << " <\033[0m\n"; // Resaltar la opción seleccionada
-        } else {
-            std::cout << "   " << opciones[i] << "\n";
-        }
-    }
-}
-void Utils::mostrarMenuCifrar(const std::string opciones[], int numOpciones, int seleccion) {
-    system("cls");
-    std::cout << "\033[35m*********** Cifrado ***********\033[0m\n";
-    for (int i = 0; i < numOpciones; ++i) {
-        if (i == seleccion) {
-            std::cout << "\033[32m > " << opciones[i] << " <\033[0m\n"; // Resaltar la opción seleccionada
-        } else {
-            std::cout << "   " << opciones[i] << "\n";
-        }
-    }
-}
-void Utils::mostrarMenuCampoCifrar(const std::string opciones[], int numOpciones, int seleccion) {
-    system("cls");
-    std::cout << "\033[35m*********** Cifrado por campo ***********\033[0m\n";
-    for (int i = 0; i < numOpciones; ++i) {
-        if (i == seleccion) {
-            std::cout << "\033[32m > " << opciones[i] << " <\033[0m\n"; // Resaltar la opción seleccionada
-        } else {
-            std::cout << "   " << opciones[i] << "\n";
-        }
-    }
-}
-
-// Validar unicidad de un campo en la lista
 bool Utils::isUniqueField(Lista_Circular_Doble<std::string>* lista, const std::string& campo, const std::string& tipo,int id) {
     Nodo<std::string>* actual = lista->cabeza;
     if (!actual) return true;
@@ -153,30 +118,30 @@ bool Utils::isUniqueField(Lista_Circular_Doble<std::string>* lista, const std::s
 
 bool Utils::isSpaceOccupied(Lista_Circular_Doble<std::string>* lista, int idEspacio) {
     Nodo<std::string>* actual = lista->getCabeza();
-    if (!actual) return true; // Si la lista está vacía, el espacio no está ocupado.
+    if (!actual) return true; 
 
     do {
         if (actual->getIdEspacio() == idEspacio) {
-            // Verificar si todos los campos están llenos.
+            
             if (actual->getCedula() != "" &&
                 actual->getPlaca() != "") {
-                return false; // El espacio está ocupado.
+                return false; 
             } else {
-                return true; // El espacio está disponible.
+                return true; 
             }
         }
         actual = actual->getSiguiente();
     } while (actual != lista->getCabeza());
 
-    return true; // No se encontró el idEspacio en la lista.
+    return true; 
 }
 
-// Definición de la función getCurrentDateTime
+
 std::string Utils::getCurrentDateTime() {
     auto now = std::chrono::system_clock::now();
     auto now_time_t = std::chrono::system_clock::to_time_t(now);
     std::string timeStr = std::ctime(&now_time_t);
-    timeStr.pop_back(); // Eliminar el salto de línea final de ctime
+    timeStr.pop_back(); 
     return timeStr;
 }
 
